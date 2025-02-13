9bd963c014d2f093b01fe3e53c2f75dfc3901df6
SanxWorkshop <69921622+SanxWorkshop@users.noreply.github.com>
2025-02-13 09:36:22 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generarExcusa() {
+    const sujetos = ["Mi gato", "Mi padre", "Una vecina", "Leticia Sabater", "Tu planta"];
+    const acciones = ["se comió", "pintó", "perdió", "rompió", "robó"];
+    const objetos = ["mis zapatos", "mi coche", "mi reloj", "mi peluca", "mi pasaporte"];
+    const lugares = ["en la calle", "en tu casa", "en el trabajo", "en Disneylandia", "en el Mercadona"];
+
+    const excusa = `${sujetos[Math.floor(Math.random() * sujetos.length)]} 
+                    ${acciones[Math.floor(Math.random() * acciones.length)]} 
+                    ${objetos[Math.floor(Math.random() * objetos.length)]} 
+                    ${lugares[Math.floor(Math.random() * lugares.length)]}.`;
+
+    document.getElementById("excuse").innerHTML = excusa;
+}
+
+window.onload = generarExcusa;
+document.getElementById("generateExcuse").addEventListener("click", generarExcusa);
+
